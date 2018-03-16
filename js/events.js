42d48b854680d3d73baba9a22c35c5cc7acc7c58
Sonia  D <sduarte22@gmail.com>
2018-03-16 02:26:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,38 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
+frameIt();
+
+sumbitIt();
+
+pressIt();
 });
+
+
+function getIt(){
+  $('p').on('click', function(clicked){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if(key.which===71){
+      alert("G!");
+    }
+  });
+}
